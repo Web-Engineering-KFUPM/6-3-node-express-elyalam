06a06d89d72b1ca118a71f4da39a1f48828f8bf2
Marloxthe legend <elyalam@github.com>
2026-04-06 18:00:27 +0300

    lab done

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -2,9 +2,12 @@
 
 // TODO 1: Define server port
 
-
+import express from "express";
+const app = express();
+const PORT = 3000;
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors);
 
 // TODO 5: Import and apply Morgan middleware
 
@@ -14,5 +17,18 @@
 
 // TODO 6.2: Create "/api/quote" route
 
+import morgan from "morgan";
+app.use(morgan("dev"));
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 // TODO 7: Start server using app.listen
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
